4782f7b8072bf24f94ac1b3d4ed6b4db551e1d3b
JayMtz <81593531+JayMtz@users.noreply.github.com>
2022-05-10 22:54:26 -0500

    Merge branch 'main' of https://github.com/JayMtz/StravaPractice into main

--- stravaapp/src/index.js
+++ stravaapp/src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Imports dependencies and sets up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+// creates express http server
+  app = express().use(bodyParser.json());
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 80, () => console.log('webhook is listening'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+  console.log("webhook event received!", req.query, req.body);
+  res.status(200).send('EVENT_RECEIVED');
+});
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+  // Your verify token. Should be a random string.
+  const VERIFY_TOKEN = "STRAVA";
+  // Parses the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Verifies that the mode and token sent are valid
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {     
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.json({"hub.challenge":challenge});  
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
